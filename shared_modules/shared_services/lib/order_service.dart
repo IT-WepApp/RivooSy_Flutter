11c2f6de285c163720ada3e59cec63ece6b95be4
ziadnasif77 <ziadnasif77@gmail.com>
2025-04-17 15:53:02 +0000

    333

--- shared_modules/shared_services/lib/order_service.dart
+++ shared_modules/shared_services/lib/order_service.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_models/shared_models.dart'; // Assuming your shared_models package
 
 class OrderService {
